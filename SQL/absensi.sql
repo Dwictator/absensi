8f16eb70da0a1e23fa7f413947c99d8bffea909b
AvocadoSmuggler <rizky.dwi9900@gmail.com>
2021-07-16 17:55:56 +0700

    Checkin & Checkout

--- SQL/absensi.sql
+++ SQL/absensi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2021 at 07:18 PM
+-- Generation Time: Jul 16, 2021 at 12:54 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id` int(99) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `date` varchar(52) NOT NULL,
+  `time` time NOT NULL,
+  `information` varchar(24) NOT NULL,
+  `status` int(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`id`, `id_user`, `date`, `time`, `information`, `status`) VALUES
+(9, 65, 'July 16, 2021', '16:58:04', 'check-in', 0),
+(10, 65, 'July 16, 2021', '17:02:01', 'check-in', 0),
+(11, 65, 'July 16, 2021', '17:08:46', 'check-out', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -40,17 +64,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `namalengkap`, `email`, `password`, `created_date`) VALUES
-(55, 'asdas@asdadasc.com', 'asdas@asdadasc.com', '$2y$10$eBwe36vmo6/qC9zd3zTxzOSNNJqxLn3m3SeSEy.4FeLKGwTS4lqBS', '2021-07-15 16:37:14'),
-(56, 'ssssss', 'asdas@asdadasc.cow', '$2y$10$Bs4OCjiH0DBAftZWxcThge54RB1JhvBCqMOTMoE2xjEvTV7ZcNZFe', '2021-07-15 16:48:36'),
-(57, 'sdfsd', 'asdsa@ada.cas', '$2y$10$/TWGmmbspYpksU1trXTCq.4Oa2l4RzAQ/Ind3wr8y9tYYnDA.L2RW', '2021-07-15 16:49:28'),
-(58, 'asds', 'asd@asd.cow', '$2y$10$igxV4zrXxrcns7MkvpCCheyWg9ZgAha8vGHNEXisDmRTIsXEa16aS', '2021-07-15 16:50:02'),
-(59, 'zxc', 'sad@adsa.asd', '$2y$10$I.9O0QIbDE9aWv2Q1FoF9uJrosRSvoywfulbOt1Li2A4veRPhQWSS', '2021-07-15 16:50:17'),
-(60, 'asdads', 'fghjkl@lkjv.ckjh', '$2y$10$d0y/61dephByy0wbN.OmDuMwUBtIxJlE7yX2QWU9JGX4/IC1YGcf6', '2021-07-15 17:02:25'),
-(61, 'Rizky Dwi Saputra', 'rizky@gmail.com', '$2y$10$zB4bS5WSh.MOkaqDIJEJSOxeTHuSEV6/rB92jIcaWmjCJQz/GJR2S', '2021-07-15 17:09:19');
+(65, 'Rizky Dwi', 'rizky@gmail.com', '$2y$10$eczfuM2fBW7Dak8ZOfHx..MdbB4mTk1gdXiGOcQijhVDjnmEhwtyC', '2021-07-16 08:58:59');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -63,10 +87,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
